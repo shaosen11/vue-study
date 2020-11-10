79fbebe93448306204536ebf8a49787dea7dd66d
Shaosen <1244785737@qq.com>
2020-11-10 22:58:05 +0800

    学习

--- vue-new-study/08-书籍购物车案例/main.js
+++ vue-new-study/08-书籍购物车案例/main.js
@@ -33,7 +33,7 @@ const app = new Vue({
         ]
     },
     methods: {
-        getFinalPrice(price){
+        getFinalPrice(price) {
             return '￥' + price.toFixed(2);
         },
         increment(index) {
@@ -42,22 +42,62 @@ const app = new Vue({
         decrement(index) {
             this.books[index].count--;
         },
-        removeHandle(index){
+        removeHandle(index) {
             this.books.splice(index, 1)
         }
     },
-    computed:{
-      totalPrice() {
-          let totalPrice = 0;
-          for (let i = 0; i < this.books.length; i++){
-              totalPrice += this.books[i].price * this.books[i].count;
-          }
-          return totalPrice;
-      }
+    computed: {
+        totalPrice() {
+            // let totalPrice = 0;
+            // for (let i = 0; i < this.books.length; i++){
+            //     totalPrice += this.books[i].price * this.books[i].count;
+            // }
+            // for (let item of this.books) {
+            //     totalPrice += item.price * item.count;
+            // }
+            // return totalPrice;
+            return this.books.reduce(function (pre, book) {
+                return pre + book.price * book.count
+            }, 0)
+        }
     },
     filters: {
-        showPrice(price){
+        showPrice(price) {
             return '￥' + price.toFixed(2);
         }
     }
 })
+
+
+//编程范式：命令编程/声明式编程
+const nums = [10, 20, 111, 222, 444, 40, 50]
+
+let total = nums.filter(n => n < 100).map(n => n * 2).reduce((pre, n) => pre + n)
+console.log(total)
+
+// let total = nums.filter(function (n) {
+//     return n < 100;
+// }).map(function (n) {
+//     return n * 2;
+// }).reduce(function (preVlalue, n) {
+//     return preVlalue + n;
+// }, 0)
+
+// //1.filter函数使用
+// let numNums = nums.filter(function (n) {
+//     return n < 100;
+// })
+// console.log(numNums)
+//
+//
+// //2.map函数的使用
+// let new2Nums = numNums.map(function (n) {
+//     return n * 2
+// })
+// console.log(new2Nums)
+//
+// //3.reduce函数使用
+// let total = new2Nums.reduce(function (preValue, n) {
+//     return preValue + n
+// }, 0)
+// console.log(total)
