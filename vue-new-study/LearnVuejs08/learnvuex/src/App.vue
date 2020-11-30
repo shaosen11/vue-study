a5844db89b4d25f12c66377d38559751e03be7dc
Shaosen <1244785737@qq.com>
2020-11-30 23:21:56 +0800

    学习

---
+++ vue-new-study/LearnVuejs08/learnvuex/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h2>{{$store.state.counter}}</h2>
+    <button @click="$store.state.counter++">+</button>
+    <button @click="$store.state.counter--">-</button>
+    <hello-vuex/>
+  </div>
+</template>
+
+<script>
+  import HelloVuex from './components/HelloVuex'
+
+export default {
+  name: 'App',
+  components:{
+    HelloVuex
+  }
+}
+</script>
+
+<style>
+
+</style>
